9828630008a2b52c3a06f79a540c6dc9e4672bad
JieGG <947295340@qq.com>
2020-05-23 13:41:04 +0800

    ⚙

---
+++ src/Core/DeveloperFramework.CQP/CQPErrorCode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeveloperFramework.CQP
+{
+	/// <summary>
+	/// 描述 CQP.dll 动态库的错误代码
+	/// </summary>
+	public static class CQPErrorCode
+	{
+		/// <summary>
+		/// 操作成功完成
+		/// </summary>
+		public const int CQP_SUCCESS = 0;
+		/// <summary>
+		/// 操作未注册
+		/// </summary>
+		public const int CQP_PROCESS_NOT_REISTER = -1;
+	}
+}
