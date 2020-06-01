264c9969b74fe61a8825352d2524c02a7e0ff7a5
JieGG <947295340@qq.com>
2020-06-01 14:15:51 +0800

    提交 getRecordV2 接口的业务逻辑

---
+++ src/Core/DeveloperFramework.Simulator.CQP/Domain/Command/GetRecordV2Command.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeveloperFramework.Simulator.CQP.Domain.Command
+{
+	public class GetRecordV2Command : AbstractCommand
+	{
+		public string File { get; }
+		public string Format { get; }
+
+		public GetRecordV2Command (CQPSimulator simulator, CQPSimulatorApp app, bool isAuth)
+			: base (simulator, app, isAuth)
+		{
+		}
+
+		public override object ExecuteHaveAuth ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public override object ExecuteHaveNoAuth ()
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
