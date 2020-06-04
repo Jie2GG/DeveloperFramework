2aa7b52eefb1fde8291bbdd1a367f3af82a77912
kotone <kotone@email.com>
2020-06-04 18:04:12 +0800

    初始化SPA

---
+++ dev/SPA/CQSim/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'CQSim';
+  constructor(firestore: AngularFirestore) {
+
+  }
+}
